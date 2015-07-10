bf014413a63046fafc06e7516a9b1217232e2d94
maxcz <maxswiat@hotmail.com>
2015-07-10 17:50:15 +0200

    added pages: faq, contact, nav1 and nav2

--- js/init.js
+++ js/init.js
@@ -6,3 +6,18 @@
   }); // end of document ready
   
 })(jQuery); // end of jQuery name space
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+
+   /* $('.collapsible-header').click(function(){
+    	var $element = $('i', $(this));
+
+    	if($(this).hasClass('active')) // then close and remove it
+    		$element.removeClass('fa-arrow-circle-o-down').addClass('fa-arrow-circle-o-up');
+    	else
+    		$element.removeClass('fa-arrow-circle-o-up').addClass('fa-arrow-circle-o-down');
+	});*/
+  });
