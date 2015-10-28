cb5c2cffccfdfa95357b6f6df16aee6d92877d89
davy engone <davy@philos.io>
2015-10-28 08:29:23 +0100

    refactored routes to use pages component

--- app/index.js
+++ app/index.js
@@ -3,47 +3,38 @@ import {Router, Route, IndexRoute} from 'react-router';
 import {render} from 'react-dom';
 import Header from './components/header';
 import Footer from './components/footer';
-import Testimonials from './components/testimonials';
-import About from './components/about-home';
-import Partner from './components/partners';
-import ProductDetail from './components/productDetails';
+import WorkshopDetail from './components/productDetails';
 import NoMatch from './components/nomatch';
-import Products from './components/products';
-import Menu from './components/menu';
+import Workshops from './components/products';
 import BootCampPage from './components/bootcamp';
-import AboutPage from './components/about';
-import CommunityPage from './components/community';
-import ContactPage from './components/contact';
+import AboutPage from './components/pages/aboutPage';
+import CommunityPage from './components/pages/communityPage';
+import ContactPage from './components/pages/contactPage';
 import FaqPage from './components/faq';
+import Philos from './components/pages/mainPage';
 
-const Philos = ()=>{
-  return <div>
-            <Header/>
-              <Testimonials/>
-              <About/>
-              <Partner/>
-            <Footer/>
+
+class Canvas extends Component{
+  render(){
+    return <div>
+             {this.props.children}
+             <Footer/>
           </div>;
-};
-
-const Canvas = ()=>{
-  return <div>
-               <Menu/>
-               {this.props.children}
-               <Footer/>
-            </div>;
+  }
 };
 
 render((<Router>
-          <Route path="/" component={Philos}/>
-          <Route path="/program" component={BootCampPage}/>
-          <Route path="/about" component={AboutPage}/>
-          <Route path="/faq" component={FaqPage}/>
-          <Route path="/community" component={CommunityPage}/>
-          <Route path="/contact" component={ContactPage}/>
-          <Route path="/products" component={Canvas}>
-            <IndexRoute component={Products}/>
-            <Route path=":id" component={ProductDetail}/>
+          <Route path="/" component={Canvas}>
+            <IndexRoute component={Philos}/>
+            <Route path="program" component={BootCampPage}/>
+            <Route path="about" component={AboutPage}/>
+            <Route path="faq" component={FaqPage}/>
+            <Route path="community" component={CommunityPage}/>
+            <Route path="contact" component={ContactPage}/>
+          </Route>
+          <Route path="/workshops" component={Canvas}>
+            <IndexRoute component={Workshops}/>
+            <Route path=":id" component={WorkshopDetail}/>
           </Route>
           <Route path="*" component={NoMatch}/>
        </Router>), document.getElementById('container'));
