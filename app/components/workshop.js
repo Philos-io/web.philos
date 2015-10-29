3ed288ad296d5f289722d0969ee19ef93e623d04
davy engone <davy@philos.io>
2015-10-29 22:07:39 +0100

    refactored the workshop module

---
+++ app/components/workshop.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router';
+import ImageCard from './image-card';
+
+const Workshop = ({...product})=>{
+ return (<Link className="upcoming-event" to={product.detailsUrl}>
+           <div className="col s12 m6 l4">
+             <div className="card">
+               {(product.image && product.displayImage && <ImageCard source={product.image}/>)}
+               <div className="card-content card-height">
+                 <p className="grey-text text-darken-2 uppercase card-title1">{product.topic}</p>
+                 <p className="grey-text text-lighten-1 card-title2">{product.length}</p>
+                 <div className="align-middle padding-top-small text-normal-minus">
+                   <span className="grey-text text-lighten-1"><i className="fa fa-calendar"></i>&nbsp;{product.date}</span>
+                </div>
+               </div>
+              </div>
+          </div>
+        </Link>);
+};
+
+export default Workshop;
--- app/components/workshops.js
+++ app/components/workshops.js
@@ -1,82 +1,13 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
-import Product from './product.js';
-import Parse from 'parse';
-import ParseReact from 'parse-react';
+
+import Product from './workshop';
 import {products} from './philos.data';
+import Workshop from './workshop';
+import Bootcamp from './bootcamp-card';
 
-let ParseComponent = ParseReact.Component(React);
-
-class Products extends ParseComponent{
-  constructor(){
-    super();
-    this.state = {
-      products : []
-    };
-    Parse.initialize("YrLdpCDNnTHrhcvjOdEu849h3A0AEeNNoRnM9qH1", "2xTgVwN2svqdkA7oawXLRyQZxlMh4ngygThCyau9");
-  }
-
-  observe(props, state) {
-    return {
-      products: new Parse.Query('Product')
-    };
-  }
-
-  render(){
-    let products = this.data.products.map((product) =>{
-      return <Product key={product.id} data={{product}}/>;
-    });
-    return <div className="row">
-            {{products}}
-            </div>;
-  }
-};
-
-let Image = ({source}) =>{
-  return <div className="card-image">
-           <img className="responsive-img" src={source} alt="AngularJS"/>
-        </div>
-};
-
-
-let Workshop = ({...product})=>{
- return (<Link className="upcoming-event" to={product.detailsUrl}>
-           <div className="col s12 m6 l4">
-             <div className="card">
-               {(product.image && product.displayImage && <Image source={product.image}/>)}
-               <div className="card-content card-height">
-                 <p className="grey-text text-darken-2 uppercase card-title1">{product.topic}</p>
-                 <p className="grey-text text-lighten-1 card-title2">{product.length}</p>
-                 <div className="align-middle padding-top-small text-normal-minus">
-                   <span className="grey-text text-lighten-1"><i className="fa fa-calendar"></i>&nbsp;{product.date}</span>
-                </div>
-               </div>
-              </div>
-          </div>
-        </Link>);
-};
-
-let Bootcamp = ({...product})=>{
- return (<Link className="upcoming-event" to={product.detailsUrl}>
-          <div className="col s12 m8 l8">
-            <div className="card">
-               {(product.image && product.displayImage && <Image source={product.image}/>)}
-              <div className="card-content card-height">
-               <p className="grey-text text-darken-2 uppercase card-title1">{product.topic}</p>
-               <p className="grey-text text-lighten-1 card-title2">{product.length}</p>
-               <div className="align-middle padding-top-small text-normal-minus">
-                 <span className="grey-text text-lighten-1"><i className="fa fa-calendar"></i>&nbsp;{product.date}</span>
-               </div>
-              </div>
-           </div>
-         </div>
-      </Link>);
-};
-
-
-class Workshops extends Component{
-  render(){
-    return <section className="grey lighten-4">
+const Workshops = ()=>{
+  return (<section className="grey lighten-4">
              <div className="container">
                <div className="row padding-top-small">
                {products.map((product)=>{
@@ -87,8 +18,7 @@ class Workshops extends Component{
                })}
                </div>
             </div>
-          </section>
-  }
+          </section>);
 };
 
 export default Workshops;
