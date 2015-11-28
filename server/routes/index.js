fa5f75027a1958cd22aa953f933e884ec26f0f45
maxcz <maxswiat@hotmail.com>
2015-11-28 21:14:18 +0100

    removed .html for evaluation form

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,10 @@ module.exports = function(app){
 		res.sendFile(path.join(dirPath+'/contact.html'));
 	});
 
+  	app.get('/evaluation-form', function(req, res){
+		res.sendFile(path.join(dirPath+'/evaluation-form.html'));
+	});
+
   	app.get('/devoxx', function(req, res){
 		res.sendFile(path.join(dirPath+'/devoxx.html'));
 	});
